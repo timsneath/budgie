2c864f9848c1cce869659945dd35c2416dac0007
Tim Sneath <tims@microsoft.com>
2016-01-01 11:04:46 +0000

    Add UWP client and unit test library

---
+++ Budgie/BudgieLibraryTest/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using BudgieLibrary;
+
+namespace BudgieLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CreateBudget()
+        {
+            var budget = new BudgetContext();
+        }
+    }
+}
